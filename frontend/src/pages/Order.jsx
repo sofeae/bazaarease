fccde089e1e69efc0f1fe2a65fcdc84862daf426
PAW\softs <sofeaezzaty@gmail.com>
2023-11-26 17:25:45 +0800

    cart hm

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Sidebar from "../components/SidebarA";
 import { Box } from "@mui/material";
-import Navbar from "../components/Navbar";
-
+import EditForm from "../components/EditForm";
 
 function Order() {
   return (
@@ -10,11 +9,6 @@ function Order() {
       <Box sx={{ display: "flex" }}>
       </Box>
       
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: "background.default", paddingLeft: 34 }}
-      >
-      </Box>
     </div>
   );
 }
